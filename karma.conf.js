8aac4559fd5464d5944da945dc875f3c71e68958
aamrr <medameurweslati@gmail.com>
2022-01-02 17:19:54 +0000

    changed travis script -d

--- karma.conf.js
+++ karma.conf.js
@@ -33,11 +33,17 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: false,
     restartOnFileChange: true
   });
